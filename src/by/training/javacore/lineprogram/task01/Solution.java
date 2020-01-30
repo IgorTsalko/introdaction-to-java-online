7ecf7e1877f8bb97540e67b97e22ae27f02fd095
Igor Tsalko <tracer.pk@mail.ru>
2020-01-30 19:10:15 +0300

    dsfsdf sdfsdf
    sdfsdf
    //
    dfdsfsd
    /**
    sdfsdf
    /**
    
    sdfsdf
    
    sdf#	modified:   src/by/training/javacore/lineprogram/task01/Solution.java

--- src/by/training/javacore/lineprogram/task01/Solution.java
+++ src/by/training/javacore/lineprogram/task01/Solution.java
@@ -9,12 +9,13 @@ public class Solution {
 		
 		double a, b, c;
 		
-		System.out.println("Расчет значения функции: z = ((a - 3) * b / 2) + c");
-		System.out.println("Введите значение переменной a: ");
+		System.out.println("Р РµС€РµРЅРёРµ СѓСЂР°РІРЅРµРЅРёСЏ: z = ((a - 3) * b / 2) + c");
+		System.out.println("Р’РІРµРґРёС‚Рµ РїРµСЂРµРјРµРЅРЅСѓСЋ a: ");
 		a = scanner.nextDouble();
-		System.out.println("Введите значение переменной b: ");
+		System.out.println("Р’РІРµРґРёС‚Рµ РїРµСЂРµРјРµРЅРЅСѓСЋ b: ");
 		b = scanner.nextDouble();
-		System.out.println("Введите значение переменной c: ");
+		System.out.println("Р’РІРµРґРёС‚Рµ РїРµСЂРµРјРµРЅРЅСѓСЋ c: ");
+		
 		c = scanner.nextDouble();
 
 		double answer = ((a - 3) * b / 2) + c;
